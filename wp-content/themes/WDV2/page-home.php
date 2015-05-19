5151c719e283107a7d2a0cff48084b036a7b0efb
berniemcgrail <berniemcgrail@gmail.com>
2015-05-19 18:20:29 +0100

    nav, hero and features responsive

--- wp-content/themes/WDV2/page-home.php
+++ wp-content/themes/WDV2/page-home.php
@@ -11,30 +11,35 @@ get_header();
 			<p class="features-feature-title">Affordable packages</p>
 			<p class="features-feature-text">No hidden extras and a simple pricing scheme.</p>
 			<a href="#" class="features-feature-link">Read More</a>
+			<div class="hr"></div>
 		</div>
 		<div class="features-feature">
 			<img src="<?php echo get_template_directory_uri(); ?>/img/icons/support.svg" alt="" class="features-feature-image">
 			<p class="features-feature-title">Unparalleled support</p>
 			<p class="features-feature-text">We’re here when you need us - online or on the phone.</p>
 			<a href="#" class="features-feature-link">Read More</a>
+			<div class="hr"></div>
 		</div>
 		<div class="features-feature">
 			<img src="<?php echo get_template_directory_uri(); ?>/img/icons/designs.svg" alt="" class="features-feature-image">
 			<p class="features-feature-title">Choice of designs</p>
 			<p class="features-feature-text">Use your own logo, colours and images.</p>
 			<a href="#" class="features-feature-link">Read More</a>
+			<div class="hr"></div>
 		</div>
 		<div class="features-feature">
 			<img src="<?php echo get_template_directory_uri(); ?>/img/icons/booking.svg" alt="" class="features-feature-image">
 			<p class="features-feature-title">Booking system</p>
 			<p class="features-feature-text">Allow your guests to book directly online - optional payment system.</p>
 			<a href="#" class="features-feature-link">Read More</a>
+			<div class="hr"></div>
 		</div>
 		<div class="features-feature">
 			<img src="<?php echo get_template_directory_uri(); ?>/img/icons/responsive.svg" alt="" class="features-feature-image">
 			<p class="features-feature-title">Mobile Optimised</p>
 			<p class="features-feature-text">Guests have all the information they need right on their phone.</p>
 			<a href="#" class="features-feature-link">Read More</a>
+			<div class="hr"></div>
 		</div>
 		<div class="features-feature">
 			<img src="<?php echo get_template_directory_uri(); ?>/img/icons/social.svg" alt="" class="features-feature-image">
@@ -52,6 +57,7 @@ get_header();
 			<img src="<?php echo get_template_directory_uri(); ?>/img/icons/affordable2.svg" alt="" class="large-features-feature-image">
 			<div class="large-features-feature-text">
 				<h3>Affordable packages</h3>
+				<div class="hr"></div>
 				<p>We have a number of packages to suit any budget. Every package includes a shiny new website using your choice of images, colours and logo.<br><br>
 				There are no hidden costs and maintenence plans ensure you’re always secrue, up to date and can make changes cheaply.</p>
 			</div>
@@ -62,6 +68,7 @@ get_header();
 			<img src="<?php echo get_template_directory_uri(); ?>/img/icons/support2.svg" alt="" class="large-features-feature-image">
 			<div class="large-features-feature-text">
 				<h3>Unparalleled support</h3>
+				<div class="hr"></div>
 				<p>We are always available to help, just send us an email or give us a call. And we won’t disappear when the website has been built, we offer support for as long as you’re with us, as well as updates, security checks, ideas to help you grow and any other support you may need.</p>
 			</div>
 		</div>
@@ -71,6 +78,7 @@ get_header();
 			<img src="<?php echo get_template_directory_uri(); ?>/img/icons/designs2.svg" alt="" class="large-features-feature-image">
 			<div class="large-features-feature-text">
 				<h3>Choice of designs</h3>
+				<div class="hr"></div>
 				<p>We know what makes a great website - it’s what we do! Together we can chose and customise a design that uses your exiting logo, colour scheme and branding, or we can make all this for you, what ever you need.<br><br>
 				We can even help with photography and editing, all to make your site stand out and be the best it can be.</p>
 			</div>
@@ -81,6 +89,7 @@ get_header();
 			<img src="<?php echo get_template_directory_uri(); ?>/img/icons/booking2.svg" alt="" class="large-features-feature-image">
 			<div class="large-features-feature-text">
 				<h3>Booking System</h3>
+				<div class="hr"></div>
 				<p>We can create a booking system for you which can be managed easily from your computer. It comes with a diary and you can take bookings directly from your website, or just recieve booking enquiries, up to you!<br><br>
 				We can also set up payments directly from your website, allowing guests to book directly, saving those commission charges from booking sites and agencies.</p>
 			</div>
@@ -91,6 +100,7 @@ get_header();
 			<img src="<?php echo get_template_directory_uri(); ?>/img/icons/responsive2.svg" alt="" class="large-features-feature-image">
 			<div class="large-features-feature-text">
 				<h3>Mobile optimised</h3>
+				<div class="hr"></div>
 				<p>We can create a booking system for you which can be managed easily from your computer. It comes with a diary and you can take bookings directly from your website, or just recieve booking enquiries, up to you!<br><br>
 				We can also set up payments directly from your website, allowing guests to book directly, saving those commission charges from booking sites and agencies.</p>
 			</div>
@@ -101,6 +111,7 @@ get_header();
 			<img src="<?php echo get_template_directory_uri(); ?>/img/icons/social2.svg" alt="" class="large-features-feature-image">
 			<div class="large-features-feature-text">
 				<h3>SEO & Social media</h3>
+				<div class="hr"></div>
 				<p>We can create a booking system for you which can be managed easily from your computer. It comes with a diary and you can take bookings directly from your website, or just recieve booking enquiries, up to you!<br><br>
 				We can also set up payments directly from your website, allowing guests to book directly, saving those commission charges from booking sites and agencies.</p>
 			</div>
